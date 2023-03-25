4f301208d59e0412e424d5e1e6b2181f0814bbf8
Michael <msousacode@gmail.com>
2023-03-24 22:24:55 -0300

    feat: create entities

---
+++ src/main/java/com/multiteam/MultiteamApplication.java
@@ -0,0 +1,13 @@
+package com.multiteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiteamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiteamApplication.class, args);
+	}
+
+}
